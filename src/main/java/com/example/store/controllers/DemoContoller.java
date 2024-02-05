e4c28fbc2b41a7b94393bb55f0426368a2b9e435
Gektorrrr <zenaslikov@gmail.com>
2024-02-05 17:58:28 +0200

    Init commit

---
+++ src/main/java/com/example/store/controllers/DemoContoller.java
@@ -0,0 +1,11 @@
+package com.example.store.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class DemoContoller {
+
+    @GetMapping("/hello")
+    public void Hello(){
+        System.out.println("Hello");
+    }
+}
