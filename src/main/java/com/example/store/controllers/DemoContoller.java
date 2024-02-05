c451e30c16ee4f72c39f1829fdb4d0ccad0ea428
Gektorrrr <zenaslikov@gmail.com>
2024-02-05 18:11:58 +0200

    Change DemoContoller

--- src/main/java/com/example/store/controllers/DemoContoller.java
+++ src/main/java/com/example/store/controllers/DemoContoller.java
@@ -1,10 +1,12 @@
 package com.example.store.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class DemoContoller {
 
-    @GetMapping("/hello")
+    @GetMapping("/user/hello")
     public void Hello(){
         System.out.println("Hellowwww");
     }
