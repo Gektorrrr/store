48180bfa7cb120f25393b2a9a2abfdac1a52420f
Gektorrrr <zenaslikov@gmail.com>
2024-02-06 14:20:17 +0200

    -Change StoreApplication,application.properties -Add AppConfig,CorsFilter

---
+++ src/main/java/com/example/store/configs/AppConfig.java
@@ -0,0 +1,17 @@
+package com.example.store.configs;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CorsFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(0);
+        return registrationBean;
+    }
+}
