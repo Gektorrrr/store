e82ba607f2c7f7a9d4479c58540ee63ddb61f432
Gektorrrr <zenaslikov@gmail.com>
2024-02-12 21:03:23 +0200

    Release V1_1

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -2,13 +2,17 @@ package com.example.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StoreApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        PasswordEncoder encoder = context.getBean(PasswordEncoder.class);
+        System.out.println(encoder.encode("pass"));
     }
 
 }
